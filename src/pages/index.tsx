65c314ecbf9623bb895c6c84c68d1118db6dafa4
Sonny <sonny.marinho@gmail.com>
2021-10-28 23:26:50 -0300

    :sparkles: feat: input component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import { Flex, Input, Button, Stack, Text, FormLabel, FormControl } from '@chakra-ui/react';
+import { Flex, Button, Stack, Text, FormLabel, FormControl } from '@chakra-ui/react';
 import type { NextPage } from 'next';
+import { Input } from '../components/Form/Input';
 
 const Home: NextPage = () => {
   return (
@@ -9,36 +10,8 @@ const Home: NextPage = () => {
       </Text>
       <Flex as="form" w="100%" maxW={360} bg="gray.800" p="8" flexDir="column" borderRadius={5}>
         <Stack spacing={4}>
-          <FormControl>
-            <FormLabel htmlFor="email">E-mail</FormLabel>
-            <Input
-              name="email"
-              type="email"
-              id="email"
-              focusBorderColor="pink.500"
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: 'gray.900',
-              }}
-              size="lg"
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="password">Password</FormLabel>
-            <Input
-              name="password"
-              type="password"
-              id="password"
-              focusBorderColor="pink.500"
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: 'gray.900',
-              }}
-              size="lg"
-            />
-          </FormControl>
+          <Input name="email" label="E-mail" type="email" />
+          <Input name="password" label="Password" type="password" />
         </Stack>
 
         <Button type="submit" mt="6" colorScheme="pink">
