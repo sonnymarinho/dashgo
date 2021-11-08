992e3759597bfebaddfce4fea706b08c09587601
Sonny Marinho <sonny.marinho@gmail.com>
2021-11-07 22:56:24 -0300

    :sparkles: feat: favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+          <link rel="icon" type="image/ico" href="/favicon.ico" />
         </Head>
         <body>
           <Main />
